12fb1f080a8d122513c551edfb27fa702bf94faa
Mkenzy Hammond <40156513+BloodyInspirations@users.noreply.github.com>
2018-06-13 15:03:42 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -12,46 +12,41 @@ practice with lists
 #Part I
 #Take a list, say for example this one:
 
-numb_name = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #and write a program that prints out all the elements of the list that are less than 5.
 
+numb_name = [2, 3, 5, 7, 8, 9, 15, 82, 63, 800, 89, -25, -5846]
 for item in numb_name:
     if item < 5:
         print item
 
 
-
-print "______"
-
-
 #Part II
 # Take two lists, say for example these two:
-b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # and write a program that creates and prints a list that contains only the elements
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
+print"________"
+m = [1, 2, 15, 5, 8, 9, 10, 7]
+l = [7, 8, 13, 14, 20, 45, 9, 10]
 
-a = [11, 22 ,3 ,6 , 8 , 9 ,15 ,10 ,12 ,4]
-b = [88, 53 ,3 ,5 , 8, 7, 15, 34, 14, 2]
-
-for item in a:
-    for item_b in b:
-        if item == item_b:
+for item in m:
+    for item_l in l:
+        if item == item_l:
             print item
-
-
-
 
 
 #Part III
 # Take a list, say for example this one:
 
-d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
+
 # and write a program that replaces all “a” with “*”.
 
+d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
+print "______"
 counter = 0
-#sum = 1
 for item in d:
     if item == "a":
         d[counter] = "*"
@@ -59,21 +54,23 @@ for item in d:
 print d
 
 
-print "________"
+
+
+
+
 
 #Part IV
-
 #Ask the user for a string, and print out whether this string is a palindrome or not.
 
-print "Hello!"
-list_one = str
-list_two = str
-str = raw_input("Choose a word and I will tell you whether it is a palindrome or not:")
-list_two.reverse
-list_one = list_two
-print ""
-
-
+dog = raw_input("Please enter a word:")
+L = []
+L2 = []
+for letter in dog:
+    L.append(letter)
+    L2.append(letter)
+print L
+L2.reverse()
+print L2
 
 
 
